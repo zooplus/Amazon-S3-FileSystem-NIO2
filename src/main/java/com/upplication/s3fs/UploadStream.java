10123f8bbc7c0c1da83884070a6ad02452f869c1
Grzegorz Gurgul <gurgul.grzegorz@gmail.com>
2018-01-23 16:25:03 +0100

    Upload batches of bytes from input stream

--- src/main/java/com/upplication/s3fs/UploadStream.java
+++ src/main/java/com/upplication/s3fs/UploadStream.java
@@ -1,7 +1,6 @@
 package com.upplication.s3fs;
 
 import com.amazonaws.services.s3.model.UploadPartRequest;
-import com.amazonaws.services.s3.model.UploadPartResult;
 import com.github.davidmoten.rx2.Bytes;
 import com.sun.tools.javac.util.Pair;
 import io.reactivex.Flowable;
@@ -9,7 +8,6 @@ import lombok.Builder;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.util.List;
 
 /**
  * https://docs.aws.amazon.com/sdkfornet1/latest/apidocs/html/M_Amazon_S3_AmazonS3Client_UploadPart.htm
@@ -25,25 +23,22 @@ public class UploadStream {
     private final UploadTaskExecutor uploadTaskExecutor;
 
     public void execute() {
-        final Flowable<byte[]> flowable = Bytes.from(inputStream, bufferSize);
-//        final Flowable<Integer> integerSeries = Flowable.generate(() -> 0, (val, tEmitter) -> {
-//            tEmitter.onNext(val + 1);
-//        });
+        Flowable.zip(
+                integerSequence(),
+                Bytes.from(inputStream, bufferSize),
+                Pair::new
+        ).map((pair) -> {
+            int partNo = pair.fst;
+            byte[] content = pair.snd;
 
-        List<UploadPartResult> list = Flowable.zip(integerSequence(), flowable, Pair::new)
-                .map((pair) -> {
-                    int partNo = pair.fst;
-                    byte[] content = pair.snd;
-
-                    return new UploadPartRequest()
-                            .withUploadId("abc")
-                            .withPartNumber(partNo)
-                            .withPartSize(content.length)
-                            .withInputStream(new ByteArrayInputStream(content));
-                    // .withBucketName(s3MultipartFileUploader.bucket)
-                    // .withKey(s3MultipartFileUploader.key)
-                })
-                .flatMap(task -> Flowable.fromCallable(() -> uploadTaskExecutor.execute(task)))
+            return new UploadPartRequest()
+                    .withUploadId("abc")
+                    .withPartNumber(partNo)
+                    .withPartSize(content.length)
+                    .withInputStream(new ByteArrayInputStream(content));
+            // .withBucketName(s3MultipartFileUploader.bucket)
+            // .withKey(s3MultipartFileUploader.key)
+        }).flatMap(task -> Flowable.fromCallable(() -> uploadTaskExecutor.execute(task)))
                 .toList()
                 .blockingGet();
     }
